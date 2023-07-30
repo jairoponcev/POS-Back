a16f82876ab1a52b37b46d98c3b413b93e9c8051
Jairo Ponce <jairo.poncegrupoleitz@gmail.com>
2023-07-29 23:41:22 -0600

    Control de excepciones y validaciones en el módulo User

--- POS.Application/Services/UserApplication.cs
+++ POS.Application/Services/UserApplication.cs
@@ -4,12 +4,14 @@ using Microsoft.IdentityModel.Tokens;
 using POS.Application.Commons.Bases;
 using POS.Application.Dtos.User.Request;
 using POS.Application.Interfaces;
+using POS.Application.Validators.User;
 using POS.Domain.Entities;
 using POS.Infrastructure.Persistences.Interfaces;
 using POS.Utilities.Static;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using WatchDog;
 using BC = BCrypt.Net.BCrypt;
 
 namespace POS.Application.Services
@@ -19,37 +21,51 @@ namespace POS.Application.Services
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly IConfiguration _configuration;
+        private readonly UserValidator _validatorRules;
 
         public UserApplication(
             IUnitOfWork unitOfWork,
             IMapper mapper,
-            IConfiguration configuration)
+            IConfiguration configuration,
+            UserValidator validatorRules)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _configuration = configuration;
+            _validatorRules = validatorRules;
         }
 
         public async Task<BaseResponse<string>> GenerateToken(TokenRequestDto requestDto)
         {
             var response = new BaseResponse<string>();
-            var account = await _unitOfWork.User.AccountByUserName(requestDto.UserName!);
 
-            if (account is not null)
+            try
             {
-                if (BC.Verify(requestDto.Password, account.Password))
-                {
-                    response.IsSuccess = true;
-                    response.Data = GenerateToken(account);
-                    response.Message = ReplyMessage.MESSAGE_TOKEN;
+                var account = await _unitOfWork.User.AccountByUserName(requestDto.UserName!);
 
-                    return response;
+                if (account is not null)
+                {
+                    if (BC.Verify(requestDto.Password, account.Password))
+                    {
+                        response.IsSuccess = true;
+                        response.Data = GenerateToken(account);
+                        response.Message = ReplyMessage.MESSAGE_TOKEN;
+
+                        return response;
+                    }
+                }
+                else
+                {
+                    response.IsSuccess = false;
+                    response.Message = ReplyMessage.MESSAGE_TOKEN_ERROR;
                 }
             }
-            else
+            catch (Exception ex)
             {
                 response.IsSuccess = false;
-                response.Message = ReplyMessage.MESSAGE_TOKEN_ERROR;
+                response.Message = ReplyMessage.MESSAGE_EXCEPTION;
+
+                WatchLogger.Log(ex.Message);
             }
 
             return response;
@@ -58,25 +74,47 @@ namespace POS.Application.Services
         public async Task<BaseResponse<bool>> RegisterUser(UserRequestDto requestDto)
         {
             var response = new BaseResponse<bool>();
-            var account = _mapper.Map<User>(requestDto);
-            account.Password = BC.HashPassword(requestDto.Password);
 
-            if (requestDto.Image is not null)
+            try
             {
-                account.Image = await _unitOfWork.Storage.SaveFile(AzureContainers.USERS, requestDto.Image);
-            }
+                var validationResult = await _validatorRules.ValidateAsync(requestDto);
 
-            response.Data = await _unitOfWork.User.RegisterAsync(account);
+                if (!validationResult.IsValid)
+                {
+                    response.IsSuccess = false;
+                    response.Message = ReplyMessage.MESSAGE_VALIDATE;
+                    response.Errors = validationResult.Errors;
 
-            if (response.Data)
-            {
-                response.IsSuccess = true;
-                response.Message = ReplyMessage.MESSAGE_SAVE;
+                    return response;
+                }
+
+                var account = _mapper.Map<User>(requestDto);
+                account.Password = BC.HashPassword(requestDto.Password);
+
+                if (requestDto.Image is not null)
+                {
+                    account.Image = await _unitOfWork.Storage.SaveFile(AzureContainers.USERS, requestDto.Image);
+                }
+
+                response.Data = await _unitOfWork.User.RegisterAsync(account);
+
+                if (response.Data)
+                {
+                    response.IsSuccess = true;
+                    response.Message = ReplyMessage.MESSAGE_SAVE;
+                }
+                else
+                {
+                    response.IsSuccess = false;
+                    response.Message = ReplyMessage.MESSAGE_FAILED;
+                }
             }
-            else
+            catch (Exception ex)
             {
                 response.IsSuccess = false;
-                response.Message = ReplyMessage.MESSAGE_FAILED;
+                response.Message = ReplyMessage.MESSAGE_EXCEPTION;
+
+                WatchLogger.Log(ex.Message);
             }
 
             return response;
---
+++ POS.Application/Validators/User/UserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using POS.Application.Dtos.User.Request;
+
+namespace POS.Application.Validators.User
+{
+    public class UserValidator : AbstractValidator<UserRequestDto>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotNull().WithMessage("El campo nombre de usuario no puede ser nulo.")
+                .NotEmpty().WithMessage("El campo nombre de usuario no puede ser vacío.");
+
+            RuleFor(x => x.Password)
+                .NotNull().WithMessage("El campo contraseña no puede ser nulo.")
+                .NotEmpty().WithMessage("El campo contraseña no puede ser vacío.");
+        }
+    }
+}
